0c966e9c9b7013970a6f324cd00b5637e9f580ff
Fredrick Gabelmann <fgabelmannjr@reticentmedia.com>
2013-02-06 01:19:36 -0600

    SHOT-11
    Integrated HockeyAppSDK and JIRA Mobile Connect for Software Quality Assurance Support.
    
    Signed-off-by: Fredrick Gabelmann <fgabelmannjr@reticentmedia.com>

---
+++ shotvibe/shotvibe/Delegates/SVInitializationBD.h
@@ -0,0 +1,15 @@
+//
+//  SVInitializationBD.h
+//  ShotVibe
+//
+//  Created by Fredrick Gabelmann on 1/23/13.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SVInitializationBD : NSObject
+
+#pragma mark - Class Methods
+
++ (void)initialize;
+@end
---
+++ shotvibe/shotvibe/Delegates/SVInitializationWS.h
@@ -0,0 +1,18 @@
+//
+//  SVInitializationWS.h
+//  ShotVibe
+//
+//  Created by Fredrick Gabelmann on 1/23/13.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SVInitializationWS : NSObject
+
+#pragma mark - Instance Methods
+
+- (void)initializeVendorLibraries;
+- (void)configureAppearanceProxies;
+- (void)processAnalytics;
+- (void)initializeLocalSettingsDefaults;
+@end
