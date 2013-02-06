562e3a7810a935de0fdaa43bd2e4b80d583f542c
Fredrick Gabelmann <fgabelmannjr@reticentmedia.com>
2013-02-06 00:11:38 -0600

    SHOT-11
    Initial Xcode project setup.
    
    Signed-off-by: Fredrick Gabelmann <fgabelmannjr@reticentmedia.com>

---
+++ shotvibe/shotvibe/AppDelegate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  shotvibe
+//
+//  Created by Fredrick Gabelmann on 2/5/13.
+//  Copyright (c) 2013 PicsOnAir Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ shotvibe/shotvibeTests/shotvibeTests.h
@@ -0,0 +1,13 @@
+//
+//  shotvibeTests.h
+//  shotvibeTests
+//
+//  Created by Fredrick Gabelmann on 2/5/13.
+//  Copyright (c) 2013 PicsOnAir Ltd. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface shotvibeTests : SenTestCase
+
+@end
