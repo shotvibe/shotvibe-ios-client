c20352aebd402f50555412867406da8514c10111
Fredrick Gabelmann <fgabelmannjr@reticentmedia.com>
2013-02-07 15:32:05 -0600

    SHOT-2
    Repaired missing directory references in Xcode project.
    
    Signed-off-by: Fredrick Gabelmann <fgabelmannjr@reticentmedia.com>

---
+++ shotvibe/Stores/SVEntityStore.h
@@ -0,0 +1,13 @@
+//
+//  SVEntityStore.h
+//  shotvibe
+//
+//  Created by Fredrick Gabelmann on 2/7/13.
+//  Copyright (c) 2013 PicsOnAir Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SVEntityStore : NSObject
+
+@end
---
+++ shotvibe/Stores/SVEntityStore.m
@@ -0,0 +1,13 @@
+//
+//  SVEntityStore.m
+//  shotvibe
+//
+//  Created by Fredrick Gabelmann on 2/7/13.
+//  Copyright (c) 2013 PicsOnAir Ltd. All rights reserved.
+//
+
+#import "SVEntityStore.h"
+
+@implementation SVEntityStore
+
+@end
