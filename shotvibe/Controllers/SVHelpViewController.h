cad0695f4a2b4d4315cea16cca103e24ae3200f9
John Gabelmann <jack.gabelmann@reticentmedia.com>
2013-04-16 17:28:05 -0400

    SHOT-4
    Implemented About view (no video file)
    
    Signed-off-by: John Gabelmann <jack.gabelmann@reticentmedia.com>

---
+++ shotvibe/Controllers/SVHelpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SVHelpViewController.h
+//  shotvibe
+//
+//  Created by John Gabelmann on 4/16/13.
+//  Copyright (c) 2013 PicsOnAir Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVHelpViewController : UIViewController
+
+@end
