6991f0b8b3056ce6ff4e6d4cb18b4e08d7b7f0c4
John Gabelmann <jack.gabelmann@reticentmedia.com>
2013-04-17 15:15:21 -0400

    SHOT-4
    * Implemented General Notification Settings
    
    Signed-off-by: John Gabelmann <jack.gabelmann@reticentmedia.com>

---
+++ shotvibe/Controllers/SVGeneralNotificationSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SVGeneralNotificationSettingsViewController.h
+//  shotvibe
+//
+//  Created by John Gabelmann on 4/17/13.
+//  Copyright (c) 2013 PicsOnAir Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVGeneralNotificationSettingsViewController : UITableViewController
+
+@end
