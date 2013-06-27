c086791d05ac765a83697f81230ad66c5d2f9abf
John Gabelmann <jack.gabelmann@reticentmedia.com>
2013-06-27 17:11:51 -0400

    Refactor
    Cleanup
    
    Signed-off-by: John Gabelmann <jack.gabelmann@reticentmedia.com>

--- shotvibe/Controllers/CaptureSelectImagesViewController.m
+++ shotvibe/Controllers/CaptureSelectImagesViewController.m
@@ -24,7 +24,6 @@
 @property (nonatomic, strong) IBOutlet UICollectionView *gridView;
 
 - (void)doneButtonPressed;
-- (void)configureGridview;
 - (void)packageSelectedPhotos:(void (^)(NSArray *selectedPhotoPaths, NSError *error))block;
 @end
 
@@ -183,112 +182,6 @@
     }
 }
 
-#pragma mark - GMGridViewDataSource
-
-/*- (NSInteger)numberOfItemsInGMGridView:(GMGridView *)gridView
-{
-    return [self.takenPhotos count];
-}
-
-
-- (CGSize)GMGridView:(GMGridView *)gridView sizeForItemsInInterfaceOrientation:(UIInterfaceOrientation)orientation
-{
-    return CGSizeMake(71, 71);
-}
-
-
-- (GMGridViewCell *)GMGridView:(GMGridView *)gridView cellForItemAtIndex:(NSInteger)index
-{
-    //NSLog(@"Creating view indx %d", index);
-    
-    CGSize size = [self GMGridView:gridView sizeForItemsInInterfaceOrientation:[[UIApplication sharedApplication] statusBarOrientation]];
-    
-    GMGridViewCell *cell = [gridView dequeueReusableCellWithIdentifier:@"GridCell"]; //[gridView dequeueReusableCell];
-    
-    if (!cell)
-    {
-        cell = [[GMGridViewCell alloc] init];
-        cell.reuseIdentifier = @"GridCell";
-    }
-    
-    [[cell.contentView subviews] makeObjectsPerformSelector:@selector(removeFromSuperview)];
-    
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
-    view.backgroundColor = [UIColor clearColor];
-    view.layer.masksToBounds = NO;
-    
-    cell.contentView = view;
-    
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:view.bounds];
-    
-    if (self.selectedGroup) {
-        
-        ALAsset *asset = [self.takenPhotos objectAtIndex:index];
-        imageView.image = [UIImage imageWithCGImage:asset.thumbnail];
-        
-    }
-    else
-    {
-        UIImage *image = [UIImage imageWithContentsOfFile:[self.takenPhotos objectAtIndex:index]];
-        
-        float oldWidth = image.size.width;
-        float scaleFactor = imageView.frame.size.width / oldWidth;
-        
-        float newHeight = image.size.height * scaleFactor;
-        float newWidth = oldWidth * scaleFactor;
-        
-        UIGraphicsBeginImageContext(CGSizeMake(newWidth, newHeight));
-        [image drawInRect:CGRectMake(0, 0, newWidth, newHeight)];
-        UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        
-        imageView.image = newImage;
-        
-    }
-    
-    [cell.contentView addSubview:imageView];
-    
-    
-    // Configure the selection icon
-    
-    UIImageView *selectedIcon = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"imageUnselected.png"]];
-    selectedIcon.userInteractionEnabled = NO;
-    selectedIcon.tag = 9001;
-    
-    if ([selectedPhotos containsObject:[self.takenPhotos objectAtIndex:index]]) {
-        
-        selectedIcon.image = [UIImage imageNamed:@"imageSelected.png"];
-    }
-    
-    selectedIcon.frame = CGRectMake(imageView.frame.size.width - selectedIcon.bounds.size.width - 5, imageView.frame.size.height - selectedIcon.bounds.size.height - 5, selectedIcon.frame.size.width, selectedIcon.frame.size.height);
-    
-    [cell.contentView addSubview:selectedIcon];
-    
-    return cell;
-}
-
-
-#pragma mark - GMGridViewActionDelegate
-
-- (void)GMGridView:(GMGridView *)gridView didTapOnItemAtIndex:(NSInteger)position
-{
-    GMGridViewCell *selectedCell = [gridView cellForItemAtIndex:position];
-    
-    for (UIImageView *imageView in selectedCell.contentView.subviews) {
-        if (imageView.tag == 9001) {
-            
-            if (![selectedPhotos containsObject:[self.takenPhotos objectAtIndex:position]]) {
-                [selectedPhotos addObject:[self.takenPhotos objectAtIndex:position]];
-                imageView.image = [UIImage imageNamed:@"imageSelected.png"];
-            }
-            else
-            {
-                [selectedPhotos removeObject:[self.takenPhotos objectAtIndex:position]];
-                imageView.image = [UIImage imageNamed:@"imageUnselected.png"];
-            }
-        }
-    }
-}*/
 
 
 #pragma mark - Private Methods
@@ -321,37 +214,6 @@
          
          [self.presentingViewController dismissViewControllerAnimated:YES completion:NULL];
      }];
-}
-
-
-- (void)configureGridview
-{
-    /*if (_gmGridView) {
-        [_gmGridView removeFromSuperview];
-        _gmGridView.actionDelegate = nil;
-        _gmGridView.dataSource = nil;
-        _gmGridView = nil;
-    }
-    
-    GMGridView *gmGridView = [[GMGridView alloc] initWithFrame:self.view.bounds];
-    gmGridView.autoresizingMask = UIViewAutoresizingFlexibleDimensions | UIViewAutoresizingFlexibleMargins;
-    gmGridView.backgroundColor = [UIColor clearColor];
-    gmGridView.centerGrid = NO;
-    
-    [gmGridView setItemSpacing:7];
-    [gmGridView setMinEdgeInsets:UIEdgeInsetsMake(7, 7, 7, 7)];
-    
-    _gmGridView = gmGridView;
-    
-    _gmGridView.dataSource = self;
-    _gmGridView.actionDelegate = self;
-    
-    [self.view addSubview:_gmGridView];
-    
-    
-    [_gmGridView reloadData];
-    
-    _gmGridView.dataSource = self;*/
 }
 
 
