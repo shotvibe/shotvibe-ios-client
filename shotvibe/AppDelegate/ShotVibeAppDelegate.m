1fd34a6b6452ad9c571dba1f733de9ac77e86a3b
Baluta Cristian <cristi.baluta@gmail.com>
2013-08-12 12:37:08 +0300

    setAllPhotosToNotNew was called too soon

--- shotvibe/AppDelegate/ShotVibeAppDelegate.m
+++ shotvibe/AppDelegate/ShotVibeAppDelegate.m
@@ -219,7 +219,7 @@ NSString * getDeviceDescription()
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 	
-	[[SVEntityStore sharedStore] setAllPhotosToNotNew];
+	//[[SVEntityStore sharedStore] setAllPhotosToNotNew];
 	NSLog(@"applicationWillResignActive fin");
 }
 
