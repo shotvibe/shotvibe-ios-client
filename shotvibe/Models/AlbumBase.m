3524b578634a58f1b8a9a7a4c1008e54f5d38b4e
benny <benny@shotvibe.com>
2013-08-20 07:53:18 +0300

    Add AlbumBase and AlbumSummary classes

---
+++ shotvibe/Models/AlbumBase.m
@@ -0,0 +1,33 @@
+//
+//  AlbumBase.m
+//  shotvibe
+//
+//  Created by benny on 8/20/13.
+//  Copyright (c) 2013 PicsOnAir Ltd. All rights reserved.
+//
+
+#import "AlbumBase.h"
+
+@implementation AlbumBase
+
+- (id)initWithAlbumId:(int64_t)albumId
+                 etag:(NSString *)etag
+                 name:(NSString *)name
+          dateCreated:(NSDate *)dateCreated
+          dateUpdated:(NSDate *)dateUpdated
+{
+    self = [super init];
+
+    if (self) {
+        _albumId = albumId;
+        _etag = etag;
+        _name = name;
+        _dateCreated = dateCreated;
+        _dateUpdated = dateUpdated;
+    }
+
+    return self;
+}
+
+
+@end
