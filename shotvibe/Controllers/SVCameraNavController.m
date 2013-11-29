3e6493b4548298dc3a4520ec227bc3ab11ea5cd1
Baluta Cristian <cristi.baluta@gmail.com>
2013-11-29 22:26:24 +0200

    fixed double back bug

--- shotvibe/Controllers/SVCameraNavController.m
+++ shotvibe/Controllers/SVCameraNavController.m
@@ -53,22 +53,23 @@
 	// Insert the AlbumGrid controller before the CameraPicker controller
 	RCLog(@"-------------------------- 2. cameraWasDismissedWithAlbum %@ %lli %lli", album, album.albumId, self.albumId);
 	
-	UIStoryboard *mainStoryboard = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
-	SVAlbumGridViewController *controller = (SVAlbumGridViewController*)[mainStoryboard instantiateViewControllerWithIdentifier:@"SVAlbumGridViewController"];
-	controller.albumManager = self.albumManager;
-	controller.albumId = albumId;
-	controller.scrollToTop = YES;
+//	UIStoryboard *mainStoryboard = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
+//	SVAlbumGridViewController *controller = (SVAlbumGridViewController*)[mainStoryboard instantiateViewControllerWithIdentifier:@"SVAlbumGridViewController"];
+//	controller.albumManager = self.albumManager;
+//	controller.albumId = albumId;
+//	controller.scrollToTop = YES;
 	
 	// Should be 2 controllers, SVAlbumListViewController and SVCameraPickerController.
 	NSMutableArray *controllers = [NSMutableArray arrayWithArray:self.nav.viewControllers];
 	RCLogO(controllers);
 	NSAssert(controllers.count>=2, @"SVCameraNavController should be 2 or 3 controllers here");
 	
-	[controllers insertObject:controller atIndex:1];
+	//[controllers insertObject:controller atIndex:1];
 	
-	[self.nav setViewControllers:controllers];
+	//[self.nav setViewControllers:controllers];
+	//RCLogO(controllers);
 	
-	// Dismiss this controller from the main navigation
+	// Dismiss SVCameraPickerController from the main navigation
 	[cameraController.navigationController popViewControllerAnimated:YES];
 	
 	NSDictionary *userInfo = @{@"albumId":[NSNumber numberWithLongLong:albumId]};
