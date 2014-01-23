ee56335afdc365c1a2ea6850e01dc39af30237cb
Oblosys <martijn@oblomov.com>
2014-01-23 12:50:08 +0100

    Fix: only take 1 line from the list of remote branches

--- tools/generateGitInfo.sh
+++ tools/generateGitInfo.sh
@@ -11,7 +11,7 @@ kShortSHA=`git rev-parse --short HEAD`
 kCurrentBranch=`git rev-parse --abbrev-ref HEAD`
 
 # A bit verbose, but doesn't fail for missing remotes
-kRemoteTracking=`git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD)`
+kRemoteTracking=`git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD) | head -1`
 
 kBuildTime=`date "+%d-%b %H:%M:%S"`
 
